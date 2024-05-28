5cc90dbd6ad40f5c49796e003fa58d3762305be2
finanovita <finanovita479@gmail.com>
2024-05-28 10:14:05 +0700

    Add project files.

---
+++ ParadigmaOOPPart2_D/ParadigmaOOPPart2_D.cpp
@@ -0,0 +1,13 @@
+// ParadigmaOOPPart2_D.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+c
+
+int main()
+{
+    
+}
+
